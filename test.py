834854c6d59b6a65b6a10087b58f61473dc9c278
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2017-01-15 19:37:44 -0800

    Dictionaries Skills Assessment

---
+++ test.py
@@ -0,0 +1,69 @@
+def kids_game(names):
+    """Play a kids' word chain game.
+
+    Given a list of names, like::
+
+      bagon baltoy yamask starly nosepass kalob nicky
+
+    Do the following:
+
+    1. Always start with the first word ("bagon", in this example).
+
+    2. Add it to the results.
+
+    3. Use the last letter of that word to look for the next word.
+       Since "bagon" ends with n, find the *first* word starting
+       with "n" in our list --- in this case, "nosepass".
+
+    4. Add "nosepass" to the results, and continue. Once a word has
+       been used, it can't be used again --- so we'll never get to
+       use "bagon" or "nosepass" a second time.
+
+    5. When you can't find an unused word to use, you're done!
+       Return the list of output words.
+
+    For example::
+
+        >>> kids_game(["bagon", "baltoy", "yamask", "starly",
+        ...            "nosepass", "kalob", "nicky", "booger"])
+        ['bagon', 'nosepass', 'starly', 'yamask', 'kalob', 'baltoy']
+
+    (After "baltoy", there are no more y-words, so we end, even
+    though "nicky" and "booger" weren't used.)
+
+    Two more examples:
+
+        >>> kids_game(["apple", "berry", "cherry"])
+        ['apple']
+
+        >>> kids_game(["noon", "naan", "nun"])
+        ['noon', 'naan', 'nun']
+
+    This is a tricky problem. In particular, think about how using
+    a dictionary (with the super-fast lookup they provide) can help;
+    good solutions here will definitely require a dictionary.
+    """
+
+
+    kid_game = {}
+    kid_games = []
+
+
+
+    # for word in names:
+    current_word = names[0]
+    print current_word
+    kid_games.append(current_word)
+    last_letter = current_word[-1]
+    # last_character = word[-1]
+    # I got stuck on this one after pulling the last letter 
+    
+    return kid_games
+
+
+
+
+
+
+kids_game(["bagon", "baltoy", "yamask", "starly",
+                   "nosepass", "kalob", "nicky", "booger"])
